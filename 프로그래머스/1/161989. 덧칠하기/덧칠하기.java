9778aa03c39bf3cf3075cc5011b09d2005a87871
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-04-18 22:49:27 +0900

    [level 1] Title: 덧칠하기, Time: 1.64 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        
+        int answer = 0;
+        int t = section[0];
+        
+        for (int i : section) {
+            if (t > n) {
+                break;
+            }
+            if (t <= i) {
+                t = i;
+                t += m;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
