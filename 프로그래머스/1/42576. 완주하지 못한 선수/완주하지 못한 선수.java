af853cc1a9ad162c52e0ed6021473dc20e0eb0d5
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-04-25 20:32:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.45 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+            
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String str : participant) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        for (String str : completion) {
+            map.put(str, map.getOrDefault(str, 0) - 1);
+        }
+        
+        for (String str : participant) {
+            if (map.get(str) > 0) {
+                return str;
+            }
+        }
+        
+        return "";
+    }
+}
