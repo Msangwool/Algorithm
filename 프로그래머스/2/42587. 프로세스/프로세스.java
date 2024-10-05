5db52f8591184a655b98c38b236cfb2303cdddc1
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-10-05 18:17:11 +0900

    [level 2] Title: 프로세스, Time: 4.56 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -6,45 +6,32 @@ class Solution {
                 
         Queue<Integer> q = new LinkedList<>();
         
-        Map<Integer, Integer> countMap = new HashMap<>();
-        Set<Integer> sortedMin = new HashSet<>();
-        
         for (int i : priorities) {
             q.add(i);
-            sortedMin.add(i);
-            countMap.put(i, countMap.getOrDefault(i, 0) + 1);
         }
         
-        List<Integer> sortedList = new ArrayList<>(sortedMin);
-        Collections.sort(sortedList, Collections.reverseOrder());
-        
-        int i = 0;
+        Arrays.sort(priorities);
+            
+        int i = priorities.length - 1;
         
         while (true) {
-            
-            if (countMap.get(sortedList.get(i)) == 0) {
-                i++;
-            }
-            
-            if (sortedList.get(i) == q.peek()) {
-                int pollQ = q.poll();
+                        
+            if (priorities[i] == q.peek()) {
+                q.poll();
+                
+                i--;
                 answer++;
-                countMap.put(pollQ, countMap.get(pollQ) - 1);
-                
-                if (location == 0) {
-                    break;
-                }
-                
                 location--;
+                
+                if (location < 0) break;
+            
                 continue;
             }
             
             q.add(q.poll());
             
             location--;
-            if (location < 0) {
-                location = q.size() - 1;
-            }
+            if (location < 0) location = q.size() - 1;
         }
     
         return answer;
