7d7977f176d45a28cfe02a33301e435d19d53e71
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-10-05 18:25:37 +0900

    [level 2] Title: 프로세스, Time: 1.29 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -6,32 +6,45 @@ class Solution {
                 
         Queue<Integer> q = new LinkedList<>();
         
+        Map<Integer, Integer> remainMinNum = new HashMap<>();
+        Set<Integer> minNum = new HashSet<>();
+        
         for (int i : priorities) {
             q.add(i);
+            minNum.add(i);
+            remainMinNum.put(i, remainMinNum.getOrDefault(i, 0) + 1);
         }
         
-        Arrays.sort(priorities);
-            
-        int i = priorities.length - 1;
+        List<Integer> sortedMinNum = new ArrayList<>(minNum);
+        Collections.sort(sortedMinNum, Collections.reverseOrder());
+        
+        int i = 0;
         
         while (true) {
-                        
-            if (priorities[i] == q.peek()) {
-                q.poll();
-                
-                i--;
-                answer++;
-                location--;
-                
-                if (location < 0) break;
             
+            if (remainMinNum.get(sortedMinNum.get(i)) == 0) {
+                i++;
+            }
+            
+            if (sortedMinNum.get(i) == q.peek()) {
+                int pollQ = q.poll();
+                answer++;
+                remainMinNum.put(pollQ, remainMinNum.get(pollQ) - 1);
+                
+                if (location == 0) {
+                    break;
+                }
+                
+                location--;
                 continue;
             }
             
             q.add(q.poll());
             
             location--;
-            if (location < 0) location = q.size() - 1;
+            if (location < 0) {
+                location = q.size() - 1;
+            }
         }
     
         return answer;
