fc4123c6e6e2710c1a8768425f565fc3a1144945
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-11-27 09:13:03 +0900

    [level 1] Title: K번째수, Time: 3.23 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -9,16 +9,9 @@ class Solution {
             int i = commands[t][0];
             int j = commands[t][1];
             int k = commands[t][2];
-            arr = new int[j - i + 1];
             
-            int q = 0;
-            for (int p = i; p < j + 1; p++) {
-                arr[q] = array[p - 1];
-                q++;
-            }
-            
+            arr = Arrays.copyOfRange(array, i - 1, j);
             Arrays.sort(arr);
-
             answer.add(arr[k - 1]); 
         }   
         
