4cc94dc4113c368bef9ed747a106230657317d03
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-12-22 16:33:12 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,9 +1,2 @@
-def solution(a, b):
-    
-    ab = int(f'{a}{b}')
-    ab2 = 2*a*b
-    
-    if (ab == ab2):
-        return ab
-    
-    return max(ab, ab2)
+def solution(a, b):    
+    return max(int(f'{a}{b}'), 2*a*b)
