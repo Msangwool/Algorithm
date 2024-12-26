decc68b261efc51b8c1028ce94faddae820574e7
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-12-26 18:56:48 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,8 +1,3 @@
 def solution(num_list):
-    num_len = len(num_list)
-    if num_list[num_len - 1] > num_list[num_len - 2]:
-        num_list.append(num_list[num_len - 1] - num_list[num_len - 2])
-        return num_list
-    
-    num_list.append(num_list[num_len - 1] * 2)
+    num_list.append(num_list[-1] - num_list[-2]) if num_list[-1] > num_list[-2] else num_list.append(num_list[-1] * 2)
     return num_list
