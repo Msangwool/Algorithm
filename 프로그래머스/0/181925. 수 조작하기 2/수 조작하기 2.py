f09dc5abe7e43022d618a67b2de651d3898a7e85
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2024-12-26 19:19:57 +0900

    [level 0] Title: 수 조작하기 2, Time: 33.25 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -2,8 +2,6 @@ def solution(numLog):
     keys = dict(zip([1, -1, 10, -10], ['w', 's', 'd', 'a']))
     answer = ''
     
-    before_num = numLog[0]
     for i in range(1, len(numLog)):
-        answer += keys[numLog[i] - before_num]
-        before_num = numLog[i]
+        answer += keys[numLog[i] - numLog[i - 1]]
     return answer
