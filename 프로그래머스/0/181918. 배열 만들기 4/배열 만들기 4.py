6c9d4eb0fc64e3dc5894b3b77bf1f352a894a7fc
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-01 18:19:41 +0900

    [level 0] Title: 배열 만들기 4, Time: 37.24 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -2,12 +2,7 @@ def solution(arr):
     i = 0
     stk = []
     while i < len(arr):
-        if not stk:
-            stk.append(arr[i])
-            i = i + 1
-            continue
-        
-        if stk[-1] < arr[i]:
+        if not stk or stk[-1] < arr[i]:
             stk.append(arr[i])
             i = i + 1
             continue
