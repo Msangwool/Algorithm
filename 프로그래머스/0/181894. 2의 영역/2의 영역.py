dd96dea0deeda3f8a5c9953bcb7d778666d5c65a
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-06 09:35:50 +0900

    [level 0] Title: 2의 영역, Time: 1.69 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,20 +1,4 @@
 def solution(arr):
-    startIdx = -1
-    endIdx = -1
-    for i in range(len(arr)):
-        if arr[i] == 2:
-            startIdx = i
-            break
-    
-    for i in range(len(arr) - 1, -1, -1):
-        if arr[i] == 2:
-            endIdx = i
-            break
-            
-    if startIdx == -1:
+    if 2 not in arr:
         return [-1]
-            
-    if endIdx == -1:
-        return arr[startIdx:]
-    
-    return arr[startIdx:endIdx+1]
+    return arr[arr.index(2):len(arr) - arr[::-1].index(2)]
--- 프로그래머스/0/181894. 2의 영역/README.md
+++ 프로그래머스/0/181894. 2의 영역/README.md
@@ -1,10 +1,10 @@
 # [level 0] 2의 영역 - 181894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 5.18 ms
+메모리: 13.4 MB, 시간: 1.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 06일 09:32:08
+2025년 01월 06일 09:35:48
 
 ### 문제 설명
 
