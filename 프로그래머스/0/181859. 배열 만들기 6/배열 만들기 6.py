524e9efc43f306b7c0653f92db8217ec4bf03b3a
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-11 16:59:32 +0900

    [level 0] Title: 배열 만들기 6, Time: 120.37 ms, Memory: 18.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    stk = []
+    for i in range(len(arr)):
+        if stk and stk[-1] == arr[i]:
+            stk.pop()
+            continue
+        
+        stk.append(arr[i])
+    return stk if stk else [-1]
