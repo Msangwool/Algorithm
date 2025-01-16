ecc7c4b3123a229c2142428e1a7e15ba0f9e483b
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-16 12:42:05 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,8 +1,6 @@
 def solution(n):
-    for i in range(1,n+1):
-        if i**2 == n:
-            return (i+1)**2
-        
-        if i**2 > n:
-            break
+    sqrt = n ** (1/2)
+    if (sqrt) % 1 == 0:
+        return (sqrt+1) ** 2
+
     return -1
