e764739c421d88e96eea64d59e0480ae114feaf5
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-17 22:41:44 +0900

    [level 0] Title: 분수의 덧셈, Time: 62.34 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,18 @@
+def solution(numer1, denom1, numer2, denom2):
+    if denom1 == denom2:
+        child = numer1 + numer2
+        parent = denom1
+    else:
+        commonParent = denom1 * denom2
+
+        child = (numer1 * commonParent//denom1) + (numer2 * commonParent//denom2)
+        parent = commonParent
+        
+    d = [i for i in range(2, parent + 1) if parent % i == 0][::-1]
+    
+    for i in d:        
+        if child % i == 0 and parent % i == 0:
+            child //= i
+            parent //= i
+            
+    return [child, parent]
