b2223b70c202c657e05bd54e0f82be40cab0cc36
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-17 22:42:35 +0900

    [level 0] Title: 분수의 덧셈, Time: 60.84 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808#) 
 
 ### 성능 요약
 
-메모리: 9.98 MB, 시간: 62.34 ms
+메모리: 10.4 MB, 시간: 60.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 17일 22:42:00
+2025년 01월 17일 22:42:27
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,19 +1,14 @@
 def solution(numer1, denom1, numer2, denom2):
-    
     commonParent = denom1 * denom2
 
     child = (numer1 * commonParent//denom1) + (numer2 * commonParent//denom2)
     parent = commonParent
         
     d = [i for i in range(2, parent + 1) if parent % i == 0][::-1]
-    print(child, parent)
-    print(d)
     
-    for i in d:
-        if child % i == 0:
+    for i in d:        
+        if child % i == 0 and parent % i == 0:
             child //= i
             parent //= i
-        
-        if i > parent:
-            break
+            
     return [child, parent]
