f66ccbc409b5f075c1f01b2c268ab6c94cc6884e
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-18 20:05:15 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,16 +1,13 @@
 def solution(n):    
-    answer = 0
-    if n % 6 == 0:
-        return n//6
-    
-    # 최소 공배수 
-    # |두 수의 곱| // 최대 공약수
-    
-    # 최대 공약수
     GCD = 0
     if n > 6:
-        GCD = max([i for i in range(1, n+1) if 6%i==0 and n%i==0])
+        GCD = getGcd(n, 6)
     else:
-        GCD = max([i for i in range(1, 7) if n%i==0 and 6%i==0])
+        GCD = getGcd(6, n)
         
-    return (abs(n*6)//GCD)//6
+    return (n*6//GCD)//6
+
+def getGcd(a, b):
+    if a % b == 0:
+        return b
+    return getGcd(b, a % b)
