6806defa5914421dc792310da0cf6e6be3a0666b
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-18 13:50:30 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,23 +1,4 @@
 def solution(array):
-#     answer = 0
-#     d = {}
-#     for i in array:
-#         if i in d:
-#             d[i] += 1
-#         else:
-#             d[i] = 1
-    
-    
-#     maxValue = max(d.values())
-#     if list(d.values()).count(maxValue) > 1:
-#         return -1
-    
-#     for i, v in d.items():
-#         if v == maxValue:
-#             answer = i
-#             break
-            
-#     return answer
     while len(array) != 0:
         for i, a in enumerate(set(array)):
             array.remove(a)
