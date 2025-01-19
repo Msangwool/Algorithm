f75e1d5b8b6ca0bd265c8c1ee776ffa09e675f0d
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-20 08:34:10 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,2 @@
 def solution(angle):
-    answer = 1
-    if angle == 90:
-        answer += 1
-    elif angle == 180:
-        answer += 3
-    elif angle > 90:
-        answer += 2
-    return answer
+    return (angle // 90) * 2 + int(angle % 90 > 0) * 1
