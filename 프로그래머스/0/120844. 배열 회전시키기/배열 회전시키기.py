e08afbe251f4ec054c9e35318b6a1c880edd2da2
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-22 08:25:34 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,15 +1,2 @@
 def solution(numbers, direction):
-    answer = [0] * len(numbers)
-    
-    if direction == 'right':
-        for i, num in enumerate(numbers):
-            answer[(i + 1) % len(numbers)] = num
-        return answer
-    
-    for i, num in enumerate(numbers):
-        if i - 1 < 0:
-            answer[len(numbers) - 1] = num
-            continue
-        
-        answer[i - 1] = num
-    return answer
+    return numbers[len(numbers)-1:] + numbers[:len(numbers)-1] if direction == "right" else numbers[1:] + numbers[0:1]
