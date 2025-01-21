9979063504d625cc3318e76cb5ea805f7603195f
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-22 00:43:47 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,11 +1,3 @@
 def solution(dot):
-    if dot[0] > 0:
-        if dot[1] > 0:
-            return 1
-        else:
-            return 4
-    
-    if dot[1] > 0:
-        return 2
-        
-    return 3
+    d = [[3, 2], [4, 1]]
+    return d[int(dot[0] > 0)][int(dot[1] > 0)]
