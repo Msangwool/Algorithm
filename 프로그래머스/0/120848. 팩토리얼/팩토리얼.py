29cbea2ebedc7bbd1400a2aaee47067ddd5e7875
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-23 20:56:32 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,9 +1,3 @@
 def solution(n):
-    f = 1
-    for i in range(1,11):
-        f *= i
-        if f == n:
-            return i
-        elif f > n:
-            return i - 1
-    return 0
+    answer = 0
+    return answer
