34eb9c9a881e9df85773505d3daf3f0044c69963
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-25 21:18:47 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,10 +1,2 @@
 def solution(s):
-    l = []
-    for i in s:
-        if s.count(i) == 1:
-            l.append(i)
-            
-    if not l:
-        return l
-    
-    return ''.join(sorted(l))
+    return ''.join(sorted(i for i in s if s.count(i) == 1))
