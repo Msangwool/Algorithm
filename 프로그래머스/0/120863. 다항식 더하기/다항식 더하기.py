4dd8510042873837c94a1ed3ea4913dd193d99d4
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-01-30 23:04:41 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,28 +1,19 @@
 def solution(polynomial):
     x1 = 0
     x0 = 0
-    for i in polynomial.split():
-        if i == '+':
+    for i in polynomial.split(' + '):
+        if i == 'x':
+            x1 += 1
             continue
-        
-        if 'x' in i:
-            if i == 'x':
-                x1 += 1
-                continue
-                
-            x1 += int(i.replace('x', ''))
+            
+        if 'x' in i:        
+            x1 += int(i[:-1])
         else:
             x0 += int(i)
             
     if x1 == 0:
-        return f'{x0}'
-    elif x0 == 0:
-        if x1 == 1:
-            return 'x'
-        
-        return f'{x1}x'
+        return str(x0)
+    elif x1 == 1:
+        return 'x' if x0 == 0 else f'x + {x0}'
     
-    if x1 == 1:
-        return f'x + {x0}'
-    
-    return f'{x1}x + {x0}'
+    return f'{x1}x' if x0 == 0 else f'{x1}x + {x0}'
