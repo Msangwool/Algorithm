29066854dc97c453824110fac13b817765268dc7
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-02-02 21:30:39 +0900

    [level 0] Title: 안전지대, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -2,19 +2,17 @@ def solution(board):
     N = len(board)
     xy = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
     
-    boom = []
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 1:
-                boom.append((i, j))
+                for x, y in xy:
+                    targetX = i + x
+                    targetY = j + y
+                    if 0 <= targetX < N and 0 <= targetY < N:
+                        if board[targetX][targetY] == 1:
+                            continue
+                        board[targetX][targetY] = 2
                 
-    for i in boom:
-        for x, y in xy:
-            iX = i[0] + x
-            iY = i[1] + y
-            if 0 <= iX < N and 0 <= iY < N:
-                board[iX][iY] = 1
-    
     answer = 0
     for i in range(len(board)):
         answer += board[i].count(0)
