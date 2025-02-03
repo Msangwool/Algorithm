1563298fbcc1c643251a6455330e275e044acd16
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-02-04 00:29:31 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,3 +1,6 @@
 def solution(sides):
-    answer = 0
-    return answer
+    # 1. n + min(sides) > max(sides) == n > max(sides) - min(sides)
+    # 2. sum(sides) > n
+    
+    # 즉, sum(sides) > n > max(sides) - min(sides)
+    return sum(sides) - (max(sides) - min(sides)) - 1
