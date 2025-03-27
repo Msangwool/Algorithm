fc3c0504bd3b7ea745871dba6bc8c545c7e06a57
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-03-27 20:18:51 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,8 +1,2 @@
-N = int(input())
-l = list(map(int, input().split()))
-v = int(input())
-cnt = 0
-for i in l:
-    if i == v:
-        cnt += 1
-print(cnt)
+int(input())
+print(list(map(int, input().split())).count(int(input())))
