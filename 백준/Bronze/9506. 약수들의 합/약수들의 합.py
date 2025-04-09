ce1b4fc04faf2839d53129ea2a161b63f9c128ab
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-04-09 22:23:37 +0900

    [Bronze I] Title: 약수들의 합, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -7,9 +7,6 @@ while 1:
     l = [i for i in range(1, n//2 + 1) if n % i == 0]
     
     if sum(l) == n:
-        print(f'{n} = ', end='')
-        for i in range(len(l) - 1):
-            print(f'{l[i]} + ', end='')
-        print(l[len(l) - 1])
+        print(f"{n} = {' + '.join(map(str, l))}")
     else:
         print(f'{n} is NOT perfect.')
