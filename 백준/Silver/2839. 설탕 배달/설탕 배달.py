efb42d1a22cf8773225dcfaa6ca6dbd19ef98339
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-04-16 22:24:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,22 +1,12 @@
 n = int(input())
 
-l = []
-
-if n % 5 == 0:
-    l.append(n//5)
-
-if n % 3 == 0:
-    l.append(n//3)
-
-for i in range(5, n, 5):
-    temp = n
-    cnt = i // 5
-    temp = temp - i
-    if temp % 3 == 0:
-        cnt += temp // 3
-        l.append(cnt)
-        
-if l:
-    print(min(l))
+cnt = 0
+while n >= 0:
+    if n % 5 == 0:
+        cnt += n//5 
+        print(cnt)
+        break
+    n -= 3
+    cnt += 1
 else:
     print(-1)
