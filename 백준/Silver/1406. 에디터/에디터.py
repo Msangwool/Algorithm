4a46028b0c0c97795c18264a6582ba71047ba5f4
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-04-23 15:07:55 +0900

    [Silver II] Title: 에디터, Time: 260 ms, Memory: 38736 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+l_stack = []
+r_stack = []
+
+for i in input().strip():
+    l_stack.append(i)
+
+for _ in range(int(input().strip())):
+    cmd = input().strip()
+    if cmd.startswith('P'):
+        a, b = cmd.split()
+        l_stack.append(b)
+    elif cmd == 'L' and l_stack:
+        r_stack.append(l_stack.pop())
+    elif cmd == 'D' and r_stack:
+        l_stack.append(r_stack.pop())
+    elif cmd == 'B' and l_stack:
+        l_stack.pop()
+
+print(''.join(l_stack) + ''.join(reversed(r_stack)))
