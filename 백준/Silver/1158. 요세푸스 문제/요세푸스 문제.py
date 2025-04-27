3f79cff73267eca98ab9b6e6e63f3c82810f01c9
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-04-27 22:32:17 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 32924 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -5,19 +5,16 @@ N, K = map(int, input().split())
 l = [i for i in range(1, N + 1)]
 
 result = []
-idx = K - 1
+idx = 0
 
 s = set()
-while 1:
+while l:
+    idx = (idx + K - 1) % len(l)
+    
     if l[idx] not in s:
         result.append(l[idx])
         s.add(l[idx])
         l.pop(idx)
-
-    if not l:
-        break
-        
-    idx = (idx + K - 1) % len(l)
 
 print("<", end="")
 print(*result, sep=", ", end="")
