1c89672f105532acc761de245d459b5a19c7303d
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-03 09:39:19 +0900

    [Silver III] Title: 소수 구하기, Time: 972 ms, Memory: 42788 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,17 +2,22 @@ import sys
 import math
 input = sys.stdin.readline
 
-def isPrimeNum(num):
-    for i in range(2, math.isqrt(num) + 1):
-        if num % i == 0:
-            return False
-    return True
-
+# 에라토스테네스 체
 M, N = map(int, input().split())
 
 if M == 1:
     M += 1
+    
+l = [True for _ in range(N + 1)]
+
+for i in range(2, N + 1):
+    if l[i] == False:
+        continue
+    j = 2
+    while i * j <= N:
+        l[i * j] = False
+        j += 1
 
 for i in range(M, N + 1):
-    if isPrimeNum(i):
+    if l[i]:
         print(i)
