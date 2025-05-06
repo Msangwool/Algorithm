8d328067a809f3d6ffb283960206088f96cc2634
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-06 13:04:10 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 144 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,30 @@
+l = ['(', ')', '[', ']']
+
+output = []
+while 1:
+    s = input()
+
+    if s == '.':
+        break
+
+    stack = []
+    for c in s:
+        if c not in l:
+            continue
+
+        if c == '(' or c == '[':
+            stack.append(c)
+            continue
+
+        if not stack:
+            output.append("no")
+            break
+
+        target = stack.pop()
+        if (c == ')' and target != '(') or (c == ']' and target != '['):
+            output.append("no")
+            break
+    else:
+        output.append("no" if stack else "yes")
+
+print('\n'.join(output))
