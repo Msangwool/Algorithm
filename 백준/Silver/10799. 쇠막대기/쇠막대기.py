7a2162683100c10286904dfa478d5487d35bf7e2
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-07 11:14:58 +0900

    [Silver II] Title: 쇠막대기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -3,18 +3,16 @@ s = sys.stdin.readline().strip()
     
 result = 0
 stack = []
-i = 0
-while i < len(s):
-    if s[i] == '(':
-        if s[i + 1] == ')':
-            result += len(stack)
-            i += 1
-        else:
-            stack.append('(')
-    elif s[i] == ')':
-        stack.pop()
-        result += 1
 
-    i += 1
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(s[i])
+        continue
+    
+    stack.pop()
+    if s[i - 1] == '(':
+        result += len(stack)
+    else:
+        result += 1
 
 print(result)
