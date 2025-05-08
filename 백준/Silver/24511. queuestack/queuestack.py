59f312bf0bdc642e86759517f25dc0bb12843919
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-08 23:49:25 +0900

    [Silver III] Title: queuestack, Time: 164 ms, Memory: 48928 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -2,19 +2,15 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-d = deque()
-
 N = int(input())
 A = list(map(int, input().strip().split()))
 B = list(map(int, input().strip().split()))
 
+M = int(input())
+C = deque(map(int, input().strip().split()))
+
 for i in range(N):
     if A[i] == 0:
-        d.appendleft(B[i])
+        C.appendleft(B[i])
 
-M = int(input())
-C = list(map(int, input().strip().split()))
-
-for i in range(M):
-    d.append(C[i])
-    print(d.popleft(), end=" ")
+print(*list(C)[:M])
