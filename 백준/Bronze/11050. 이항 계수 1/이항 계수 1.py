12f95bcb9f8ee303b4359a68057906ffca317cd4
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-09 09:00:00 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,18 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-def factorial_v1(num, cnt):
-    result = 1
-    for i in range(cnt):
-        result *= num
-        num -= 1
-    return result 
-
-def factorial_v2(num, limit):
-    if num < limit:
+def factorial(num):
+    if num <= 1:
         return 1
-    return num * factorial_v2(num - 1, limit)
+    return num * factorial(num - 1)
 
 N, K = map(int, input().strip().split())
 
-print(factorial_v1(N, K) // factorial_v2(K, 1))
+print(factorial(N) // (factorial(N - K) * factorial(K)))
