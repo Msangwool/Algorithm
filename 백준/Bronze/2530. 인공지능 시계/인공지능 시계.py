c58bc6e3b80c1171a921f01a780189e6c396f8e0
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-16 12:08:25 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -4,21 +4,5 @@ input = sys.stdin.readline
 A, B, C = map(int, input().strip().split())
 D = int(input())
 
-A_tmp = D // 3600
-D = D % 3600
-B_tmp = D // 60
-D = D % 60
-
-C += D
-if C >= 60:
-    C %= 60
-    B_tmp += 1
-    
-B += B_tmp
-if B >= 60:
-    B %= 60
-    A_tmp += 1
-    
-A = (A + A_tmp) % 24
-
-print(A, B, C)
+second = A * 3600 + B * 60 + C + D
+print((second // 3600) % 24, (second % 3600) // 60, (second % 3600) % 60)
