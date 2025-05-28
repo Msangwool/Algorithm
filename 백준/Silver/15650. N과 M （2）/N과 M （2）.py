7f9528570c5be48d70432ef16214c94563c20ced
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-05-28 19:32:53 +0900

    [Silver III] Title: N과 M (2), Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,21 +1,25 @@
 N, M = map(int, input().split())
+
 visited = [False] * (N + 1)
 result = []
-hash = set()
+s = set()
 
 def backtrack():
-    temp = tuple(sorted(result))
-    if len(result) == M and temp not in hash:
-        print(*result)
-        hash.add(temp)
+    if len(result) == M:
+        t = tuple(sorted(result))
+        if t in s:
+            return
+        
+        print(' '.join(map(str, result)))
+        s.add(t)
         return
-
+    
     for i in range(1, N + 1):
         if not visited[i]:
             visited[i] = True
             result.append(i)
             backtrack()
-            result.pop()
             visited[i] = False
-
+            result.pop()
+            
 backtrack()
