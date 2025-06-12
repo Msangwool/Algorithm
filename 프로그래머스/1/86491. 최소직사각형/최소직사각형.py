0d3707ed3807348d17681166b9df41fd9833c28e
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-06-12 16:37:46 +0900

    [level 1] Title: 최소직사각형, Time: 3.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,13 +1,2 @@
 def solution(sizes):
-    row = []
-    col = []
-    
-    for size in sizes:
-        if size[0] > size[1]:
-            row.append(size[0])
-            col.append(size[1])
-        else:
-            row.append(size[1])
-            col.append(size[0])
-            
-    return max(row) * max(col)
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
