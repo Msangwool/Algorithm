876aff95ddbb7009f697d24a5a54308910e3e3a5
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-08-08 23:46:20 +0900

    [Silver IV] Title: 토너먼트, Time: 188 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int round = 0;
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+
+        System.out.println(round);
+    }
+}
