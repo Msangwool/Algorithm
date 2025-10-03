77640521c4020c9e9d3f55a06be8c997ffafd9b0
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-10-03 13:49:27 +0900

    [level 2] Title: 주식가격, Time: 34.07 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 32.46 ms
+메모리: 75.2 MB, 시간: 34.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 02일 17:23:26
+2025년 10월 03일 13:49:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,23 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
+        int pricesLen = prices.length;
+        int[] answer = new int[pricesLen];
+        
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int idx = stack.pop();
-                answer[idx] = i - idx; 
+        
+        // 1. 각 인덱스에 맞게 걸린 시간을 계산해야 함
+        // 2. stack을 써서 while로 비어질 때까지 계산하면서 기존에 들어있던 애가 더 크면 걔는 점수가 떨어진 것
+        // 3. stack에 저장하는 값은 해당 값이 아니라 인덱스를 저장해야 함
+        //    - 인덱스로 저장해야 해당 인덱스에 지난 초과 시간을 저장할 수 있음
+        
+        // 반복
+        for (int i = 0; i < pricesLen; i++) {
+            // while 돌면서 stack이 없을 떄까지 돌아야 함. 즉, 비어있으면 계속 돌아야 함
+            // 추가로 스택이 현재보다 큰지를 검사해야 함, 크면 걔는 빼고 그 인덱스로 숫자 넣어줘야 함
+            // 즉, 반대로 작을때
+            while (!stack.empty() && prices[stack.peek()] > prices[i]) {
+                int j = stack.pop(); // 가격이 떨어진 인덱스
+                // 떨어진 시간은 j부터 i까지 센기간
+                answer[j] = i - j;
             }
             stack.push(i);
         }
-
-        while (!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = n - 1 - idx;
+        
+        // 마지막까지 초가 떨어지지 않은 애들은 추가를 해줘야 함
+        while (!stack.empty()) {
+            int j = stack.pop();
+            answer[j] = pricesLen - j - 1; // 길이는 인덱스보다 1이 크니까 -1을 해줘야 함
         }
-
         return answer;
     }
 }
