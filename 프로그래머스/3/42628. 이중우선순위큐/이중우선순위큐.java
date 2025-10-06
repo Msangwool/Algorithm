d90efffd57848abab729c3651b112bfbf6b88b91
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-10-06 15:04:14 +0900

    [level 3] Title: 이중우선순위큐, Time: 109.87 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,32 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = {};
-        TreeMap<Integer, Integer> tree = new TreeMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         
         for (int i = 0; i < operations.length; i++) {
             String[] parts = operations[i].split(" ");
             String operation = parts[0];
             int num = Integer.parseInt(parts[1]);
-                        
+            
             if (operation.equals("I")) {
-                tree.put(num, tree.getOrDefault(num, 0) + 1);
-            } else {
-                // 연산 무시
-                if (tree.isEmpty()) continue;
+                // insert 되는 값을 key로 삽입, 들어오는 갯수를 value로 삽입
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            } else if (operation.equals("D")) {
+                if (map.isEmpty()) continue;
                 
-                int key = (num == -1) ? tree.firstKey() : tree.lastKey();
-                int value = tree.get(key);
-                
-                if (value == 1) {
-                    tree.remove(key);
-                } else {
-                    value -= 1;
-                    tree.put(key, value);
+                int key = (num == 1) ? map.lastKey() : map.firstKey();
+                // -1 하기 전의 value가 1이면 제거
+                if (map.put(key, map.get(key) - 1) == 1) {
+                    map.remove(key);
                 }
             }
         }
         
-        if (tree.isEmpty()) return new int[] {0, 0};
-        return new int[] {tree.lastKey(), tree.firstKey()};
+        if (map.isEmpty()) return new int[]{0, 0};
+        return new int[]{map.lastKey(), map.firstKey()};
     }
 }
