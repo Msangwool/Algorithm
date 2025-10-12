3c66a735e69d67b0de9599d0d08f2fe90cef13a1
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-10-12 19:10:48 +0900

    [level 2] Title: 피로도, Time: 3.16 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 83.7 MB, 시간: 5.22 ms
+메모리: 80.2 MB, 시간: 3.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 10일 08:35:43
+2025년 10월 12일 19:10:43
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,5 +1,7 @@
 class Solution {
+    
     private boolean[] visited;
+    
     private int answer;
     
     public int solution(int k, int[][] dungeons) {
@@ -7,19 +9,17 @@ class Solution {
         backtrack(k, dungeons, 0);
         return answer;
     }
-    
-    private void backtrack(int k, int[][] dungeons, int count) {
-        answer = Math.max(answer, count);
+
+    private void backtrack(int k, int[][] dungeons, int cnt) {
+        answer = Math.max(answer, cnt);
         
         for (int i = 0; i < dungeons.length; i++) {
-            int need = dungeons[i][0];
-            int cost = dungeons[i][1];
-            
-            if (!visited[i] && k >= need) {
+            if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                backtrack(k - cost, dungeons, count + 1);
-                visited[i] = false;            
+                backtrack(k - dungeons[i][1], dungeons, cnt + 1);
+                visited[i] = false;
             }
         }
     }
+   
 }
