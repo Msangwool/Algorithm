d03f374aa992b5d46ceac7554e25e588648e9fcb
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-10-22 23:44:33 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
 
 ### 성능 요약
 
-메모리: 9.13 MB, 시간: 0.04 ms
+메모리: 94.2 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 20일 21:31:58
+2025년 10월 22일 23:44:30
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        for (int i = 0; i < len; i++) {
+            char ch = name.charAt(i);
+            int move = Math.min(ch - 'A', 26 - (ch - 'A'));
+            answer += move;
+        }
+
+        int move = len - 1;  
+        for (int i = 0; i < len; i++) {
+            int next = i + 1;
+            while (next < len && name.charAt(next) == 'A') {
+                next++;
+            }
+            move = Math.min(move, i + len - next + Math.min(i, len - next));
+        }
+
+        answer += move;
+        return answer;
+    }
+}
