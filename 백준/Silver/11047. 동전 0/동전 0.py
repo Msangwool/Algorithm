20ad0447305d606d55acd3d4c2824496dab03ca0
OpenTheDoor <97933061+Msangwool@users.noreply.github.com>
2025-10-24 20:12:32 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+
+count = 0
+
+for coin in reversed(coins):
+    if K == 0:
+        break
+    count += K // coin      
+    K %= coin    
+
+print(count)
